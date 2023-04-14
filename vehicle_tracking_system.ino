8707b2e5e64881ab012c49d7a62c8b4b118fe897
Kuldeep Aher <76651819+kuldeepaher01@users.noreply.github.com>
2023-04-15 02:38:51 +0530

    Updated for ESP
    Changed directions
    Now new values are appended to firebase

--- vehicle_tracking_system.ino
+++ vehicle_tracking_system.ino
@@ -1,44 +1,48 @@
+//Including important libraries
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 #include <Firebase_ESP_Client.h>
 
-// Provide the token generation process info.
+// Provide the token generation process info
 #include "addons/TokenHelper.h"
-// Provide the RTDB payload printing info and other helper functions.
+// Provide the RTDB payload printing info and other helper functions
 #include "addons/RTDBHelper.h"
 
-static const int RXPin = 4, TXPin = 5; // GPIO 4=D2(conneect Tx of GPS) and GPIO 5=D1(Connect Rx of GPS
-static const uint32_t GPSBaud = 9600;  // if Baud rate 9600 didn't work in your case then use 4800
+static const int RXPin = 16, TXPin = 17;  // GPIO 4=D2(conneect Tx of GPS) and GPIO 5=D1(Connect Rx of GPS)
+static const uint32_t GPSBaud = 9600;     // setting GPS Baud rate
 
-TinyGPSPlus gps; // The TinyGPS++ object
+TinyGPSPlus gps;                  // The TinyGPS++ object
+SoftwareSerial ss(RXPin, TXPin);  // The serial connection to the GPS device
 
-SoftwareSerial ss(RXPin, TXPin); // The serial connection to the GPS device
 
-float speed;      // Variable  to store the speed
-float altitude;   // Variable  to store the altitude
-float sats;       // Variable to store no. of satellites response
-String direction; // Variable to store orientation or direction of GPS
+float speed;       // Variable  to store the speed
+float altitude;    // Variable  to store the altitude
+float sats;        // Variable to store no. of satellites response
+String direction;  // Variable to store orientation or direction of GPS
 float latitude, longitude;
+float prevlatitude, prevlongitude;
+float angle;
+int first =1;
 int year, month, date, hour, minute, second;
 String date_str, time_str;
 
 int pm;
 
 // Insert your network credentials
-const char *ssid = "abcd";
-const char *password = "iotpassword";
+const char *ssid = "BORGIR 2G";
+const char *password = "Sattychomu";
 
 // Insert Firebase project API Key
-#define API_KEY "apikey"
+#define API_KEY "put_ur_api"
 
 // Insert Authorized Email and Corresponding Password
-#define USER_EMAIL "kuldeep.aher212@vit.edu"
-#define USER_PASSWORD "*****"
+#define USER_EMAIL "kuldeep@gmail.coom"
+#define USER_PASSWORD "Admin@123"
 
-// Insert RTDB URLefine the RTDB URL
-#define DATABASE_URL "https://nodemcu-gps-****-default-rtdb.asia-southeast1.firebasedatabase.app/"
+// Insert RTDB URLefine the RTDB URL https://
+#define DATABASE_URL "https://abcd.firebaseio.com/"
 
 // Define Firebase objects
 FirebaseData fbdo;
@@ -56,7 +60,7 @@ String altPath = "/altitude";
 String longPath = "/longitude";
 String speedPath = "/speed";
 String timePath = "/timestamp";
-String directionPath = "/direction";
+String anglePath = "/heading";
 
 // Parent Node (to be updated in every loop)
 String parentPath;
@@ -66,32 +70,14 @@ FirebaseJson json;
 // Variable to save current epoch time
 String timestamp;
 
-// Timer variables (send new readings every three minutes)
-unsigned long sendDataPrevMillis = 0;
-unsigned long timerDelay = 10000;
-
-// // Initialize WiFi
-// void initWiFi() {
-//   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-//   Serial.print("Connecting to WiFi ..");
-//   while (WiFi.status() != WL_CONNECTED) {
-//     Serial.print('.');
-//     delay(1000);
-//   }
-//   Serial.println(WiFi.localIP());
-//   Serial.println();
-// }
-
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-  ss.begin(9600);
+  ss.begin(9600);  //Begin UART communication between GPS and NodeMCU
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -113,7 +99,7 @@ void setup()
   fbdo.setResponseSize(4096);
 
   // Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+  config.token_status_callback = tokenStatusCallback;  // see addons/TokenHelper.h
 
   // Assign the maximum retry of token generation
   config.max_token_generation_retry = 5;
@@ -123,8 +109,7 @@ void setup()
 
   // Getting the user UID might take a few seconds
   Serial.println("Getting User UID");
-  while ((auth.token.uid) == "")
-  {
+  while ((auth.token.uid) == "") {
     Serial.print('.');
     delay(1000);
   }
@@ -137,65 +122,63 @@ void setup()
   databasePath = "/UsersData/" + uid + "/data";
 }
 
-void loop()
-{
-  while (ss.available() > 0)
-  {
-    // sketch displays information every time a new sentence is correctly encoded.
-    if (gps.encode(ss.read()))
-    {
+void loop() {
+  Serial.println(ss.available());
+  while (ss.available() > 0) {
+    // Everytime successfull connection is established btweeen geo-satellites this code will run
+    if (gps.encode(ss.read())) {
       displayInfo();
-      timeStmp();
+      if(prevlatitude != latitude || prevlongitude != longitude || first == 1)
+      {timeStmp();
       postToFB();
-      delay(10000);
+      delay(4000);}
     }
   }
 }
 
-void postToFB()
-{
+void postToFB() {
   // Send new readings to database
-  if (Firebase.ready())
-  {
-    sendDataPrevMillis = millis();
-
-    // //Get current timestamp
+  first = 0;
+  if (Firebase.ready()) {
+    // Get current timestamp
     timestamp = date_str + time_str;
     Serial.print("time: ");
     Serial.println(timestamp);
 
-    parentPath = databasePath + "/" + String(timestamp);
+    parentPath = databasePath + "/";
 
     json.set(latPath.c_str(), latitude);
     json.set(longPath.c_str(), longitude);
     json.set(speedPath.c_str(), speed);
     json.set(altPath.c_str(), altitude);
-    json.set(directionPath.c_str(), String(direction));
+    json.set(anglePath.c_str(),angle);
+    // json.set(directionPath.c_str(), String(direction));
     json.set(timePath.c_str(), timestamp);
-    Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
+    
+    Serial.printf("Set json... %s\n", Firebase.RTDB.pushJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
   }
 }
 
-void displayInfo()
-{
+void displayInfo() {
+  prevlatitude = latitude;
+  prevlongitude = longitude;
   Serial.print(gps.location.isValid());
-  // if (gps.location.isValid()) {
-  latitude = (gps.location.lat()); // Storing the Lat. and Lon.
+  latitude = (gps.location.lat());  // Storing the Lat. and Lon.
   longitude = (gps.location.lng());
   Serial.print("LAT:  ");
-  Serial.println(latitude, 6); // float to x decimal places
+  Serial.println(latitude, 6);  // float to x decimal places
   Serial.print("LONG: ");
   Serial.println(longitude, 6);
-  speed = gps.speed.kmph(); // get speed
+  speed = gps.speed.kmph();  // get speed
   altitude = gps.altitude.meters();
-  sats = gps.satellites.value();                         // get number of satellites
-  direction = TinyGPSPlus::cardinal(gps.course.value()); // get the direction
-  Serial.println(direction);
-  // }
+  sats = gps.satellites.value();
+  // get number of satellites
+  angle = gps.course.deg();  // get the angle of direction in degrees
+  Serial.print("Angle: ");
+  Serial.println(angle, 2);
 }
 
-void timeStmp()
-{
+void timeStmp() {
   if (gps.date.isValid())
 
   {
@@ -206,15 +189,15 @@ void timeStmp()
     if (year < 10)
       date_str += '0';
     date_str += String(year);
-    date_str += "/";
+    date_str += "-";
     if (month < 10)
       date_str += '0';
     date_str += String(month);
-    date_str += "/";
+    date_str += "-";
     if (date < 10)
       date_str = '0';
     date_str += String(date);
-    date_str += "/";
+    date_str += "-";
   }
 
   if (gps.time.isValid())
@@ -225,8 +208,7 @@ void timeStmp()
     minute = gps.time.minute();
     second = gps.time.second();
     minute = (minute + 30);
-    if (minute > 59)
-    {
+    if (minute > 59) {
       minute = minute - 60;
       hour = hour + 1;
     }
